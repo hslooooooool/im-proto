9334a76f655280a89047cffd09a1a8fd940f68aa
华清松 <821034742@qq.com>
2019-12-13 10:49:48 +0800

    commit

--- protoc.bat
+++ protoc.bat
@@ -1,10 +1,11 @@
 @echo off
 for %%i in (.\config\*.proto) do (
     protoc --csharp_out=..\src\main\java %%i
-    rem 从这里往下都是注释，可忽略
-    echo From %%i To %%~ni.cs Successfully!
+    rem
+    echo From %%i To %%~ni.cs OK!
+
     protoc --java_out=..\src\main\java %%i
-    rem 从这里往下都是注释，可忽略
-    echo From %%i To %%.java Successfully!
+    rem
+    echo From %%i To %%.java OK!
 )
 pause
