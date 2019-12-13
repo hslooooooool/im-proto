04957cdd35d9e30ffd2ffe266513cca6cdce1940
华清松 <821034742@qq.com>
2019-12-13 11:08:04 +0800

    修改目录

--- protoc.bat
+++ protoc.bat
@@ -1,11 +1,12 @@
 @echo off
-for %%i in (.\config\*.proto) do (
-    protoc --csharp_out=..\src\main\java %%i
-    rem
-    echo From %%i To %%~ni.cs OK!
 
-    protoc --java_out=..\src\main\java %%i
-    rem
-    echo From %%i To %%.java OK!
+cd config
+
+for %%i in (*.proto) do (
+    protoc --csharp_out=../csharp %%i
+    protoc --java_out=../java %%i
+    rem 从这里往下都是注释，可忽略
+    echo %%i OK
 )
+
 pause
