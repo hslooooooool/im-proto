2b9f5947247e975aabdcab32588410b95bd74567
华清松 <821034742@qq.com>
2019-12-19 13:07:31 +0800

    整理

--- protoc.bat
+++ protoc.bat
@@ -5,6 +5,7 @@ cd config
 for %%i in (*.proto) do (
     protoc --csharp_out=../csharp %%i
     protoc --java_out=lite:../java %%i
+    protoc --js_out=import_style=commonjs,binary:../js %%i
     rem 从这里往下都是注释，可忽略
     echo %%i OK
 )
